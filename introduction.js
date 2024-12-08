6fd47011ac3b4aea7f1f2a4626be776ffcd9d024
Ayeshaa-Taahir <ayesha.tahir.dhanoooo@gmail.com>
2024-12-08 19:44:27 +0500

    start

---
+++ introduction.js
@@ -0,0 +1,8 @@
+Hello Ayesha Tahir here
+I made a Fitness Tracker Web on frontend with my team
+Fitness Tracker Web :-
+It is a responsive and interactive fitness tracking website designed to help users achieve their health and fitness goals. This project includes features like  workout suggestions, calorie tracking, and progress visualization using HTML, CSS, and JavaScript.
+Goals of the Project :-
+1-Help users stay motivated by tracking their fitness journey.
+2-Provide easy-to-use tools for health and wellness management.
+3-Enhance my skills in front-end development using JavaScript.
