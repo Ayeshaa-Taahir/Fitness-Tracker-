d1e57debeb3009aa300382f3e556f62d17197512
Bakhtawar29580 <kbakhtawar341@gmail.com>
2024-12-10 02:00:17 +0500

    Done by Manal

--- introduction.js
+++ introduction.js
@@ -1,8 +1,9 @@
-Hello Ayesha Tahir here
-I made a Fitness Tracker Web with my team
-Fitness Tracker Web :-
-It is a responsive and interactive fitness tracking website designed to help users achieve their health and fitness goals. This project includes features like  workout suggestions, calorie tracking, and progress visualization using HTML, CSS, and JavaScript.
-Goals of the Project :-
-1-Help users stay motivated by tracking their fitness journey.
-2-Provide easy-to-use tools for health and wellness management.
-3-Enhance my skills in front-end development using JavaScript.
+document.addEventListener('DOMContentLoaded', () => {
+    const featureList = document.querySelectorAll('#features li');
+  
+    featureList.forEach((item) => {
+      item.addEventListener('click', () => {
+        alert(You clicked on: ${item.textContent});
+      });
+    });
+  });
