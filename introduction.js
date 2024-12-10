c77fcaeac745bdf62ad1dd884823c9c9f7f47ad1
Ayeshaa-Taahir <ayesha.tahir.dhanoooo@gmail.com>
2024-12-10 23:10:21 +0500

    done by Ayesha

--- introduction.js
+++ introduction.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const featureList = document.querySelectorAll('#features li');
-  
-    featureList.forEach((item) => {
+  const featureList = document.querySelectorAll('#features li');
+
+  featureList.forEach((item) => {
       item.addEventListener('click', () => {
-        alert(You clicked on: ${item.textContent});
+          alert(`You clicked on: ${item.textContent}`);
       });
-    });
   });
+});
